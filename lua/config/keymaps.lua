8090dbd4b30d32e1b339473d79a0bd64b0b646de
Digvijay Mahapatra <thediggu@gmail.com>
2024-01-04 00:14:27 +0530

    feat: Add config files

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>?", require("telescope.builtin").live_grep, { desc = "[?] Find in all files" })
+vim.keymap.set("n", "<leader>/", function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+  }))
+end, { desc = "[/] Fuzzily search in current buffer" })
+vim.keymap.set(
+  "n", -- mode
+  "<leader>fp", -- key to bind
+  function()
+    vim.fn.setreg("*", vim.fn.expand("%")) -- copy file path to clipboard
+    vim.notify("File path copied to clipboard", vim.log.levels.INFO) -- display notification
+  end,
+  { noremap = true, silent = true, desc = "Copy file path relative to root" } -- options
+)
